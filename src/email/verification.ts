2edb58efa21b384a957f6fac5095c3725f2d2919
Djibril Mugisho <mugishodjibril7@gmail.com>
2024-04-24 23:56:11 +0300

    Update verification.ts

--- src/email/verification.ts
+++ src/email/verification.ts
@@ -7,6 +7,7 @@ export const emailVerificationTemplate = (verificationKey: string): string => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
     </head>
+
     
     <body style="
     background: rgba(251, 167, 251, 0.09);
